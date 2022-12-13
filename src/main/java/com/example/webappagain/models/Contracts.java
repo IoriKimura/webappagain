557524adbb14de8ef9e02bdf62e3eb61e95d10e2
Serge <sergeykonstwork@gmail.com>
2022-12-13 15:09:54 +0300

    deleting some trash

--- src/main/java/com/example/webappagain/models/Contracts.java
+++ src/main/java/com/example/webappagain/models/Contracts.java
@@ -14,18 +14,14 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Contracts {
-
     @Column
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contract_id;
-
     @Column
     private Long task_id;
-
     @Column
     private String equipment_id;
-
     public Contracts (Long task_id, String equipmentID){
        this.task_id = task_id;
        this.equipment_id = equipmentID;
--- src/main/java/com/example/webappagain/service/UserService.java
+++ src/main/java/com/example/webappagain/service/UserService.java
@@ -1,15 +1,10 @@
 package com.example.webappagain.service;
 
-import com.example.webappagain.models.Employee;
 import com.example.webappagain.repository.EmployeeRepo;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService{
